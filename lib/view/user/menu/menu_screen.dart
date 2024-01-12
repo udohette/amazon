aac24f8ed407b9b01d0542d4d19fa62577318cfc
udohette <udohette@gmail.com>
2024-01-12 10:27:57 +0100

    first layer completed on menu  screen

--- lib/view/user/menu/menu_screen.dart
+++ lib/view/user/menu/menu_screen.dart
@@ -23,55 +23,87 @@ class _MenuScreenState extends State<MenuScreen> {
       appBar:  PreferredSize(
           preferredSize: Size(width * 1, height * 0.08),
           child: HomePageAppBar(width: width, height: height)),
-      body: Container(
-        height: height,
-        width: width,
-        padding: EdgeInsets.symmetric(vertical: height * 0.02, horizontal: width * 0.03),
-        decoration: BoxDecoration(
-          gradient: LinearGradient(colors: appBarGradientColor, begin: Alignment.topLeft, end: Alignment.bottomRight)
-        ),
-        child: Column(children: [
-          Container(
-            padding: EdgeInsets.all(5),
-            height:  height * 0.1,
-            decoration: BoxDecoration(
-              color: Colors.white,
-              borderRadius: BorderRadius.all(Radius.circular(10)),
-              border: Border.all(color: greyShade3),
-            ),
-            child: Column(
-              children: [
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Expanded(
-                      flex: 2,
-                      child: Container(
-                        height: height * 0.07,
-                        width: width * 0.4,
-                        decoration: BoxDecoration(
-                            color: Colors.amberAccent.shade100
+      body: SingleChildScrollView(
+        physics: const NeverScrollableScrollPhysics(),
+        child: Container(
+          height: height,
+          width: width,
+          padding: EdgeInsets.symmetric(vertical: height * 0.02, horizontal: width * 0.03),
+          decoration: BoxDecoration(
+            gradient: LinearGradient(colors: appBarGradientColor, begin: Alignment.topLeft, end: Alignment.bottomRight)
+          ),
+          child: Column(children: [
+            Container(
+              padding: EdgeInsets.all(5),
+              height:  height * 0.1,
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.all(Radius.circular(10)),
+                border: Border.all(color: greyShade3),
+              ),
+              child: Column(
+                children: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Expanded(
+                        flex: 2,
+                        child: Container(
+                          height: height * 0.07,
+                          width: width * 0.4,
+                          decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(7),
+                              color: Colors.amberAccent.shade400
+                          ),
                         ),
+                      ),
+                      CommonFunctions.blankSpace(0, width * 0.02),
+
+                      Expanded(
+                        flex: 2,
+                        child: Container(
+                          height: height * 0.07,
+                          width: width * 0.4,
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(7),
+                              color: Colors.amber.shade100
+                          ),
+                        ),
+                      )
+                    ],
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                    children: [
+                    Text('Amazon Pay', style: textTheme.bodyMedium,),
+                    Text('Amazon miniTV',style: textTheme.bodyMedium,),
+                  ],),
+                ],
+              ),
+            ),
+            CommonFunctions.blankSpace(height * 0.01, 0),
+            GridView.builder(
+              physics: const PageScrollPhysics(),
+              scrollDirection: Axis.vertical,
+                shrinkWrap: true,
+                itemCount: 18,
+                gridDelegate:  const SliverGridDelegateWithFixedCrossAxisCount(
+                    crossAxisCount: 3,
+                    crossAxisSpacing: 10,
+                    mainAxisSpacing: 10,
+                childAspectRatio: 0.8),
+                itemBuilder: (context,index){
+                  return Expanded(
+                    child: Container(
+                      decoration: BoxDecoration(
+                          color: Colors.amber,
+                        borderRadius: BorderRadius.circular(7)
                       ),
                     ),
-                    CommonFunctions.blankSpace(0, width * 0.02),
-
-                    Expanded(
-                      flex: 2,
-                      child: Container(
-                        height: height * 0.07,
-                        width: width * 0.4,
-                        decoration: BoxDecoration(
-                            color: Colors.amber.shade100
-                        ),
-                      ),
-                    )
-                  ],
-                ),
-              ],
-            ),
-          )
-        ],),
+                  );
+                })
+          ],),
+        ),
       ),
     )
     );
